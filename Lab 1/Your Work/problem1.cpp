6df57b8c906ce806c9bcdd5736db933a5431c4b1
Suhelcse <mdsuhel261.096@gmail.com>
2026-09-09 11:34:57 +0600

    Add files via upload

---
+++ Lab 1/Your Work/problem1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int a = 10;
+int *ptr = &a;
+
+
+
+cout <<"value of a: "<< a << endl;
+cout<<"Memory address of a:"<<ptr<<endl;
+
+return 0;
+
+}
